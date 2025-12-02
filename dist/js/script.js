36134779452409cfa9ab3930208f097eeec281d4
ThoriqAR1301 <thoriqar1301@gmail.com>
2025-12-02 15:43:14 +0700

    Sertikom

--- dist/js/script.js
+++ dist/js/script.js
@@ -7,25 +7,3 @@ window.addEventListener("scroll", () => {
 const overlay = document.getElementById("popupOverlay");
 const closeBtn = document.getElementById("closePopupBtn");
 const buttons = document.querySelectorAll('.button');
-
-// buka popup
-buttons.forEach(btn => {
-  btn.addEventListener("click", (e) => {
-    e.preventDefault();
-    overlay.style.display = "flex";
-  });
-});
-
-// tutup popup
-closeBtn.onclick = () => overlay.style.display = "none";
-
-// klik area gelap → tutup
-overlay.onclick = (e) => {
-  if (e.target === overlay) overlay.style.display = "none";
-};
-
-// debug submit
-document.getElementById("popupForm").addEventListener("submit", (e) => {
-  e.preventDefault();
-  alert("Form berhasil dikirim!");
-});
